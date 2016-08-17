186624e5c65641d04af907f5bf9e348a7078dd8c
duzhengjun_github <duzjqhu@aliyun.com>
2016-08-17 11:30:15 +0800

    framework built finish

---
+++ XnNationalDefenseMobilize/XnNationalDefenseMobilize/Controllers/GrassrootWorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XnNationalDefenseMobilize.Controllers
+{
+    public class GrassrootWorkController : Controller
+    {
+        //
+        // GET: /GrassrootWork/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ XnNationalDefenseMobilize/XnNationalDefenseMobilize/Controllers/PolicyAdviceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XnNationalDefenseMobilize.Controllers
+{
+    public class PolicyAdviceController : Controller
+    {
+        //
+        // GET: /PolicyAdvice/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

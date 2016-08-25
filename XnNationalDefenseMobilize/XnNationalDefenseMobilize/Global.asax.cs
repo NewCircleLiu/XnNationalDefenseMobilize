78c08887f06a8fa19b59eddf2241a9a78b7c0ccf
Zhengjun-Du <duzjqhu@aliyun.com>
2016-08-25 14:31:16 +0800

    back_end views and front_end views are classfied

--- XnNationalDefenseMobilize/XnNationalDefenseMobilize/Global.asax.cs
+++ XnNationalDefenseMobilize/XnNationalDefenseMobilize/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using XnNationalDefenseMobilize.App_Start;
 
 namespace XnNationalDefenseMobilize
 {
@@ -22,6 +23,14 @@ namespace XnNationalDefenseMobilize
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RegisterView();
+        }
+
+        protected void RegisterView()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new MyViewEngine());
         }
     }
 }

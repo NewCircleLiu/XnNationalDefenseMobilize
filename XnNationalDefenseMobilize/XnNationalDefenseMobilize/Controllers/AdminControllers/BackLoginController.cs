a1189752a99d6ede08e1673ef5cfb8f66122370b
Zhengjun-Du <duzjqhu@aliyun.com>
2016-11-18 01:38:08 +0800

    modify front end and back end login page

--- XnNationalDefenseMobilize/XnNationalDefenseMobilize/Controllers/AdminControllers/BackLoginController.cs
+++ XnNationalDefenseMobilize/XnNationalDefenseMobilize/Controllers/AdminControllers/BackLoginController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using XnNationalDefenseMobilize.Controllers.AdminControllers;
 using XnNationalDefenseMobilize.Models.User;
 
 namespace XnNationalDefenseMobilize.Controllers.BackControllers
